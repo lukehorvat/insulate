07e183740afca3b82296df2206eb56570396cba3
Luke Horvat <lukehorvat@gmail.com>
2013-07-15 02:09:32 +1000

    Cleaned up and simplified JS injection code.

--- lib/insulate/action_controller_filters.rb
+++ lib/insulate/action_controller_filters.rb
@@ -3,35 +3,28 @@ module Insulate
     def self.included base
       base.module_eval do
         prepend_view_path "#{Insulate.root}/app/views/"
-        after_filter :update_callback, :if => :html_response_from_render?
+        after_filter :inject_callback_script, :if => :html_response_from_render?
       end
     end
+
+    protected
 
     def html_response_from_render?
       [nil, 'text/html'].include?(response.content_type) && self.status != 302
     end
 
-    def update_callback
-      insulate_script = view_context.render(
+    def inject_callback_script
+      callback_script = view_context.render(
         :partial => 'insulate/callback',
-        :locals => {
-          :controller => controller_path,
-          :action => action_name
-      })
+        :locals => { :controller => controller_path, :action => action_name })
 
-      before_body_end_index = response_body[0].rindex('</body>')
-
-      if before_body_end_index.present?
-        before_body_end_content = response_body[0][0, before_body_end_index].html_safe
-        after_body_end_content = response_body[0][before_body_end_index..-1].html_safe
-
-        response_body[0] = before_body_end_content + insulate_script + after_body_end_content
-
-        response.body = response_body[0]
+      body_closing_tag_index = response_body[0].rindex('</body>')
+      if body_closing_tag_index.present?
+        # Insert callback_script just before </body> tag
+        response_body[0].insert body_closing_tag_index, callback_script
       else
-        # If body tag is not present, append insulate_script in the response body
-        response_body[0] += insulate_script
-        response.body = response_body[0]
+        # Append callback_script in the response body
+        response_body[0].concat callback_script
       end
     end
   end

83aa068afb599bacb78514d5176d3e794ddef7d3
Luke Horvat <lukehorvat@gmail.com>
2013-07-17 22:12:12 +1000

    Implemented body class modifier.

--- lib/insulate/action_controller_filters.rb
+++ lib/insulate/action_controller_filters.rb
@@ -14,10 +14,11 @@ module Insulate
 
     def inject_script
       doc = Nokogiri::HTML response.body
+
       head = doc.at_css 'head'
       if head
         # inject the following script as the first child of the <head>
-        script = doc.create_element 'script', script_contents(controller_path, action_name), :type => 'text/javascript'
+        script = doc.create_element 'script', head_script(controller_path, action_name), :type => 'text/javascript'
 
         head_children = head.children
         if head_children.empty?
@@ -25,16 +26,27 @@ module Insulate
         else
           head_children.before script
         end
-
-        # replace the response
-        response.body = doc.to_html
       end
+
+      body = doc.at_css 'body'
+      if body
+        # append the following class to the <body>
+        body['class'] ||= ''
+        body['class'] = body['class'] << ' ' << body_class(controller_path, action_name)
+      end
+
+      # replace the response
+      response.body = doc.to_html
     end
 
     private
 
-    def script_contents(controller, action)
-      "window.INSULATE_PAGE_ID='#{controller}##{action}';"
+    def head_script(controller, action)
+      "window.INSULATE_PAGE='#{controller}##{action}';"
+    end
+
+    def body_class(controller, action)
+      "insulate-page-#{controller}-#{action}"
     end
   end
 
